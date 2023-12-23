274c15ad8fce8446bc38987daa54cf3df12ab420
harshit1042 <105489003+harshit1042@users.noreply.github.com>
2023-12-23 16:07:35 +0530

    constants.js
    resume link update

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,7 +11,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/harshit1042",
   resume:
-    "https://drive.google.com/file/d/18Pwecrmm1OXE44ovDn5lb8AzZChLGl63/view?usp=sharing",
+    "https://drive.google.com/file/d/1L3CrSj9WhcLnWznvOA7br-XH1UhRBii8/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/harshit-chodvadiya-a12388232?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
   twitter: "https://x.com/Harshit_1042?t=Eyl7Mkx1xMBAxzLusSFx_A&s=09",
   insta: "https://www.instagram.com/harshit_402/",
